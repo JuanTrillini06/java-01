69cd3434e1a2055efcbb03cfdac3c42a4cb0bd0b
JuanTrillini06 <juanjose_207@hotmail.com>
2025-04-08 21:11:59 -0300

    Clase 08 completa para generar la base de datos y cargar datos

--- clase-08/src/main/java/com/coderhouse/Clase08Application.java
+++ clase-08/src/main/java/com/coderhouse/Clase08Application.java
@@ -1,13 +1,59 @@
 package com.coderhouse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Clase08Application {
+import com.coderhouse.dao.DaoFactory;
+import com.coderhouse.models.Alumno;
+import com.coderhouse.models.Curso;
 
+@SpringBootApplication
+public class Clase08Application implements CommandLineRunner{
+	
+	@Autowired
+	private DaoFactory dao;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Clase08Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		try {
+			
+			Curso curso1 = new Curso("Java");
+			Curso curso2 = new Curso("JavaScript");
+			Curso curso3 = new Curso("React");
+			Curso curso4 = new Curso("Angular");
+			Curso curso5 = new Curso("SQL");
+			
+			Alumno alumno1 = new Alumno("Alejandro", "Di Stefano", 22333666, "L22333666", 49);
+			Alumno alumno2 = new Alumno("Alejandro", "Di Stefano", 33555888, "L33555888", 49);
+			Alumno alumno3 = new Alumno("Alejandro", "Di Stefano", 44222666, "L44222666", 49);
+			Alumno alumno4 = new Alumno("Alejandro", "Di Stefano", 77999444, "L77999444", 49);
+			Alumno alumno5 = new Alumno("Alejandro", "Di Stefano", 11555777, "L11555777", 49);
+			
+			
+			dao.persistirCurso(curso1);
+			dao.persistirCurso(curso2);
+			dao.persistirCurso(curso3);
+			dao.persistirCurso(curso4);
+			dao.persistirCurso(curso5);
+			
+			dao.persistirAlumno(alumno1);
+			dao.persistirAlumno(alumno2);
+			dao.persistirAlumno(alumno3);
+			dao.persistirAlumno(alumno4);
+			dao.persistirAlumno(alumno5);
+			
+		}catch(Exception err) {
+			err.printStackTrace(System.err);
+		}
+		
+		
 	}
 
 }
